05ea90745e248bed356406276e11166528031ee6
Kaustubh Joshi <kaustubh19j@gmail.com>
2019-10-06 18:49:24 +0530

    New project on todo list using js

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+$("ul").on("click","li",function() {
+$(this).toggleClass("done");
+});
+
+$("ul").on("click","span",(function(event)
+{
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    })
+    event.stopPropagation();
+}));
+
+$("input[type='text']").keypress(function(event)
+{
+    if(event.which===13)
+    {
+     var task = $(this).val();
+     $(this).val("");
+     $("ul").append("<li>"+"<span><i class='fas fa-trash-alt'></i></span>"+" "+task+"</li>");
+    }
+})
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();  
+})
